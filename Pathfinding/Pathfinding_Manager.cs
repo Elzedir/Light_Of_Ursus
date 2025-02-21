6aa85f8837eb5d2996a57a2bddfdab72fea5d0d1
Elzedir <ndj.smith.96@gmail.com>
2025-02-21 01:47:53 -0300

    Using triangulation for NavMesh

---
+++ Pathfinding/Pathfinding_Manager.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Pathfinding
+{
+    public class Pathfinding_Manager
+    {
+        readonly Graph_World _graph_World = new();
+        readonly Grid_Node _grid_Node = new();
+        readonly Graph_NavMesh _graph_NavMesh = new();
+
+        public List<Vector3> GetPath(Vector3 start, Vector3 end, HashSet<MoverType> moverTypes)
+        {
+            var worldPath = _graph_World.FindShortestPath(start, end);
+
+            if (worldPath == null || worldPath.Count == 0)
+                return null;
+            
+            _showVisibleVoxels(worldPath);
+            
+            //if (!within distance of player) return;
+            
+            var localStart = worldPath.Last();
+
+            var localPath = moverTypes.Contains(MoverType.Air) || moverTypes.Contains(MoverType.Dig)
+                ? _grid_Node.FindShortestPath(localStart, end)
+                : _graph_NavMesh.FindShortestPath(localStart, end);
+            
+            _showVisibleVoxels(localPath);
+
+            return localPath;
+
+            //* Instead of running DStarLite from start to end, instead run it from individual node to node, so it's limited
+            //* in size per character. Also, pass this path through to each character, and their individual DStarLte pathfinders
+            //* will navigate their small circles around them.
+        }
+        
+        void _showVisibleVoxels(List<Vector3> path)
+        {
+            var visibleVoxelsParent = GameObject.Find("VisibleVoxels").transform;
+            var mesh = Resources.GetBuiltinResource<Mesh>("Cube.fbx");
+            var green = Resources.Load<Material>("Materials/Material_Green");
+            var red = Resources.Load<Material>("Materials/Material_Red");
+            var blue = Resources.Load<Material>("Materials/Material_Blue");
+            
+            var materials = new List<Material> { green, red };
+            
+            Debug.Log($"Path Length: {path.Count}");
+            
+            foreach (var point in path)
+            {
+                _showVoxel(visibleVoxelsParent, point, mesh, materials, Vector3.one, 0);
+            }
+        }
+
+        static void _showVoxel(Transform parentTransform, Vector3 position, Mesh mesh, List<Material> materials, Vector3 size, int colorIndex)
+        {
+            var material = materials[colorIndex % materials.Count]; 
+            
+            var voxelGO = _testShowVoxel(parentTransform, position, mesh, material, size);
+            voxelGO.name = $"Merged: {position} - {size}";
+        }
+
+        static GameObject _testShowVoxel(Transform transform, Vector3 position ,Mesh mesh, Material material, Vector3 size)
+        {
+            var voxelGO = new GameObject($"{position}");
+            voxelGO.AddComponent<MeshFilter>().mesh = mesh;
+            voxelGO.AddComponent<MeshRenderer>().material = material;
+            voxelGO.transform.SetParent(transform);
+            voxelGO.transform.localPosition = position;
+            voxelGO.transform.localScale = size;
+            return voxelGO;
+        }
+    }
+}
